c81c8995043211b6df93441ae239e827f7b7e196
idyllchow <idyllzhou@gmail.com>
2017-03-15 23:15:02 +0800

    add search page

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,9 +1,20 @@
-Page({data: {
+Page({
+  data: {
     focus: false,
     inputValue: ''
   },
-  bindButtonTap: function() {
-    this.setData({
-      focus: true
-    })
-  },})
+  // bindButtonTap: function() {
+  //   this.setData({
+  //     focus: true
+  //   })
+  // },
+  bindKeyInput: function(e) {
+    this.setData({inputValue: e.detail.value})
+    console.log("input value: " + e.detail.value )
+  },
+
+  searchBook:function(event){
+    console.log("====inputValue===" + this.inputValue)
+  }
+
+  })
